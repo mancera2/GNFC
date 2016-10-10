13cd25650e310a00297754dd56791605cbef36a7
mancera2 <natia.mgeladze.1@iliauni.edu.ge>
2016-10-11 00:09:38 +0400

    add library

--- app/src/main/java/ge/dev/gnfc/gnfc/MainActivity.java
+++ app/src/main/java/ge/dev/gnfc/gnfc/MainActivity.java
@@ -3,11 +3,15 @@ package ge.dev.gnfc.gnfc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new DrawerBuilder().withActivity(this).build();
     }
 }
